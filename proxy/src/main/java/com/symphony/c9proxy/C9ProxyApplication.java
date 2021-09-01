b3bbe9deac5fc21dd23e8c139ec1744cffaf5d6b
johanblumenberg <johan.blumenberg@symphony.com>
2021-09-01 15:49:49 +0200

    Add proxy project

---
+++ proxy/src/main/java/com/symphony/c9proxy/C9ProxyApplication.java
@@ -0,0 +1,13 @@
+package com.symphony.c9proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C9ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C9ProxyApplication.class, args);
+	}
+
+}
---
+++ proxy/src/test/java/com/symphony/c9proxy/C9ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symphony.c9proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C9ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
