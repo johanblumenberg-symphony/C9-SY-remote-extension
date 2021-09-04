03a20086bee2cd26899faf2e93f046342fca2248
johanblumenberg <johan.blumenberg@symphony.com>
2021-09-04 19:43:08 +0200

    Page numbers in main page

--- client/src/ui/main/MainView.tsx
+++ client/src/ui/main/MainView.tsx
@@ -6,19 +6,23 @@ import { withStyles, WithStyles } from '@material-ui/core/styles';
 import { styles } from '../styles';
 import { connect, ConnectedComponent } from '@symphony/rtc-react-state';
 import { C9Store } from '../../c9/C9Store';
+import { MainPagePresenter } from '../../presentation/MainPagePresenter';
 
 class MainView extends ConnectedComponent<WithStyles<typeof styles>> {
     private _c9Store = this.context.get(C9Store.TypeTag);
+    private _main = this.context.get(MainPagePresenter.TypeTag);
 
     depsToState() {
         return {
+            pageNo: this._main.getPage(),
             buttons: this._c9Store.getButtons(),
         };
     }
 
     private _getColumnButtons(col: number): (Button | undefined)[] {
-        const { buttons = [] } = this.depsToState();
-        const result: (Button | undefined)[] = buttons.slice(8 * col, 8 * col + 8);
+        const { buttons = [], pageNo } = this.depsToState();
+        const firstIndex = 16 * pageNo + 8 * col;
+        const result: (Button | undefined)[] = buttons.slice(firstIndex, firstIndex + 8);
         for (let i = result.length; i < 8; i++) {
             result.push(undefined);
         }
